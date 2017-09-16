0e6bfa716dbb23941b02426df7488fb4988c20da
洪文乐 <noclip@foxmail.com>
2017-09-16 08:55:24 +0800

    计分，记最高分

--- main.js
+++ main.js
@@ -48,9 +48,14 @@ function clickFly() {
         bird.body.velocity.y = -320;
 }
 function start () {
+    webs.forEachAlive(function (web) {
+        web.kill();
+    }, this);
     bird.reset(game.width / 4, game.height / 2);
     webs.setAll('x', -50);
     btStart.visible = false;
+    score = 0;
+    textScore.setText(score + '/' + high);
 }
 function createWeb() {
     var offsetY = 50 + Math.random() * (game.height - 130 - 150 -100) | 0;
@@ -61,6 +66,7 @@ function createWeb() {
             offsetY
         );
         web.body.velocity.x = -150;
+        web.scored = -1;
     }
     web = webs.getFirstExists(false);
     if (web) {
@@ -74,8 +80,22 @@ function createWeb() {
 function update () {
     game.physics.arcade.collide(bird, floor, gameOver, null, this);
     game.physics.arcade.collide(bird, webs, gameOver, null, this);
+    if (bird.alive) {
+        webs.forEachAlive(function (web) {
+            if (web.scored == -1 && web.x < bird.x) {
+                score++;
+                textScore.setText(score + '/' + high);
+                web.scored = 1;
+            }
+        }, this);
+    }
 }
 function gameOver (bird, killer) {
     bird.kill();
     btStart.visible = true;
+    if (score > high) {
+        high = score;
+        localStorage.setItem('bird_high', high);
+        textScore.setText(score + '/' + high);
+    }
 }
